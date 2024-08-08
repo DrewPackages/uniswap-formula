3b66dc78ed9ccf0ed7612b86820d0e445a88ea78
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-08-09 00:59:38 +0300

    fix: optional version

--- formula.js
+++ formula.js
@@ -10,7 +10,7 @@ function deploy(params) {
   api.offchain.deploy({
     details: {
       envs: {
-        UNISWAP_WEB_VERSION: params.version,
+        UNISWAP_WEB_VERSION: params?.version || undefined,
       },
       flags: {
         build: true,
